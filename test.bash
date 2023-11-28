cba966bf9a570667518df2c4a2888d078addb822
saawaa <saawaa0050@gmail.com>
2023-11-29 01:06:17 +0900

    test.yml chenge 15.0 -> 14.0

--- test.bash
+++ test.bash
@@ -1,5 +1,16 @@
 #!/bin/bash
+#SPDX-FileCopyrightText: 2022 Shusuek Osawa
+#SPDX-License-Identifier: BSD-3-Clause
+
+ng (){
+	echo NG at line $1
+	res=1
+}
+
+res=0
 
 out=$(seq 5|./calculator.py)
+[ "${out}" = 14.0 ] || ng ${LINENO}
 
-[ "${out}" = 15.0 ]
+[ "$res" = 0 ] && echo OK
+exit $res
