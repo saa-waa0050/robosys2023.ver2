37b1cab223df0c51047fe141631e570353e6969d
saawaa <saawaa0050@gmail.com>
2023-11-29 23:15:36 +0900

    add times & avg

--- test.bash
+++ test.bash
@@ -21,4 +21,34 @@ out=$(echo |./plus)
 [ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
+
+
+
+out=$(seq 5|./times)
+[ "${out}" = 120 ] || ng ${LINENO}
+
+out=$(echo あ|./times)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo |./times)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+
+
+
+out=$(seq 5|./avg)
+[ "${out}" = 3 ] || ng ${LINENO}
+
+out=$(echo あ|./avg)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo |./avg)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
 exit $res
