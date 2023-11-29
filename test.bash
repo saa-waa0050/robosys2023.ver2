6771a4da99fc98f3d7d5305e8cf73e2c6b94ace9
saawaa <saawaa0050@gmail.com>
2023-11-29 21:37:29 +0900

    file name calculator.py -> puls

--- test.bash
+++ test.bash
@@ -9,14 +9,14 @@ ng (){
 
 res=0
 
-out=$(seq 5|./calculator.py)
+out=$(seq 5|./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
-out=$(echo あ|./calculator.py)
+out=$(echo あ|./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
-out=$(echo |./calculator.py)
+out=$(echo |./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
