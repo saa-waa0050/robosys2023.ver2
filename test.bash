cbf266382321730e6ccf59300958008141511767
saawaa <saawaa0050@gmail.com>
2023-11-30 10:10:39 +0900

    test.bash add other test

--- test.bash
+++ test.bash
@@ -16,7 +16,18 @@ res=0
 out=$(seq 5|./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
+out=$(tail -n 2 ans_log.txt|head -n 1)
+[ "${out}" = "sum-> 15" ] || ng ${LINENO}
+
 out=$(echo あ|./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo @|./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo /n|./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
@@ -30,7 +41,18 @@ out=$(echo |./plus)
 out=$(seq 5|./times)
 [ "${out}" = 120 ] || ng ${LINENO}
 
+out=$(tail -n 2 ans_log.txt|head -n 1)
+[ "${out}" = "times-> 120" ] || ng ${LINENO}
+
 out=$(echo あ|./times)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo @|./times)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo /n|./times)
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
@@ -45,7 +67,18 @@ out=$(echo |./times)
 out=$(seq 5|./avg)
 [ "${out}" = 3 ] || ng ${LINENO}
 
+out=$(tail -n 2 ans_log.txt|head -n 1)
+[ "${out}" = "avg-> 3" ] || ng ${LINENO}
+
 out=$(echo あ|./avg)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo @|./avg)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo /n|./avg)
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
